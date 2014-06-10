94af090e421e01744889d08c6f634ab91e6fa35e
Bijay Rungta <bijay.rungta@jabong.com>
2014-06-10 13:26:33 +0530

    Add Commands for GitHub Setup

--- githubSetup.sh
+++ githubSetup.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+function usage()
+{
+    echo "You can run this script by running below command"
+    echo "wget https://raw.githubusercontent.com/bijayrungta/utility-scripts/master/githubSetup.sh"
+    echo "bash githubSetup.sh"
+}
+
 function doGenerateSshKeys()
 {
     echo "Generating SSH Keys. Press Enter when prompted for Passphrase."
     ssh-keygen -t rsa -C "${userEmail}" -f id_rsa
     ssh-add ~/.ssh/id_rsa
+    doPublishSshKeys
+}
 
+function doPublishSshKeys()
+{
     echo "Install xclip if Ubuntu"
     sudo apt-get install xclip
     xclip -sel clip < ~/.ssh/id_rsa.pub
@@ -24,7 +35,7 @@ read userEmail
 # if [[ ! "${userName}" ] -o [ ! "${userName}" ]]; then
 if [ ! "${userName}" -o ! "${userEmail}" ]; then
     echo 'Can not proceed without name or email'
-    return;
+    exit;
 fi
 
 echo "Set Global GIT Config for User name and Email"
@@ -38,4 +49,10 @@ originalDir=$(pwd)
 cd ~/.ssh
 if [ ! -f ~/.ssh/id_rsa ]; then
     doGenerateSshKeys
+else
+    echo "You already have a SSH Key in your Home"
+    doPublishSshKeys
 fi
+
+echo "Go back to Original Directory"
+cd ${originalDir}
