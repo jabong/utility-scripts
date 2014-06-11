b5ef75f0a517295c91f28d9cbd80153ca1c4854a
Bijay Rungta <bijay.rungta@jabong.com>
2014-06-11 18:38:17 +0530

    Fix Script for GitHub Setup.

--- githubSetup.sh
+++ githubSetup.sh
@@ -24,14 +24,15 @@ function doPublishSshKeys()
     sudo apt-get install xclip
     xclip -sel clip < ~/.ssh/id_rsa.pub
 
-    echo "#######################################"
+    echo ${separator}
     echo "## Please visit the link below to Add your SSH key to GitHub"
+    echo "## https://github.com/settings/ssh"
     echo "## https://help.github.com/articles/generating-ssh-keys#step-3-add-your-ssh-key-to-github"
+    echo ${separator}
 }
 
 function doCaptureUserInfo()
 {
-
     echo "Set Global GIT Config for User name and Email if not set already"
     userName="$(git config --global user.name)"
     userEmail="$(git config --global user.email)"
@@ -47,22 +48,33 @@ function doCaptureUserInfo()
     fi
 }
 
-if [ ! "${userName}" -o ! "${userEmail}" ]; then
-    echo 'Can not proceed without name or email'
-    exit;
-fi
+function runSetup()
+{
+    originalDir=$(pwd)
+    echo ${separator}
+    echo 'Welcometo Jabong!! This script will help you get started with Development for Jabong'.
+    echo "Create the SSH Directory if it doesn't exist"
+    mkdir -p ~/.ssh
 
-echo "Create the SSH Directory if it doesn't exist"
-mkdir -p ~/.ssh
+    doCaptureUserInfo
 
-originalDir=$(pwd)
-cd ~/.ssh
-if [ ! -f ~/.ssh/id_rsa ]; then
-    doGenerateSshKeys
-else
-    echo "You already have a SSH Key in your Home"
-    doPublishSshKeys
-fi
+    if [ ! "${userName}" -o ! "${userEmail}" ]; then
+        echo 'Can not proceed without name or email'
+        exit;
+    fi
 
-echo "Go back to Original Directory"
-cd ${originalDir}
+    if [ ! -f ~/.ssh/id_rsa ]; then
+        doGenerateSshKeys
+    else
+        echo "You already have a SSH Key in your Home"
+        doPublishSshKeys
+    fi
+
+    echo "Go back to Original Directory"
+    cd ${originalDir}
+}
+
+separator="#########################################################################"
+
+runSetup
+
